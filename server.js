badb0c738d8fb8906268a09bb2bf85c666bbc5d6
Michael Stromer <stroms38@gmail.com>
2019-10-04 12:27:37 -0400

    random endpoint

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const PORT = process.env.PORT || 3000;
 
-const { Prompts } = require('./models');
+const { Prompts, sequelize } = require('./models');
 
 const app = express();
 
@@ -22,4 +22,11 @@ app.get('/prompts/:id', async (req, res) => {
     const id = parseInt(req.params.id)
     const prompt = await Prompts.findByPk(id)
     res.json(prompt)
+})
+
+app.get('/random', async (req, res) => {
+	const prompt = await Prompts.findOne({ 
+	  order: sequelize.random() 
+	});
+	res.json(prompt)
 })
